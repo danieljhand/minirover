d5b4244bfbedc658f60a776a323a8391bdb87f1b
Daniel J Hand <danieljhand@yahoo.com.au>
2018-08-29 22:01:43 +0800

    added to do comments for speed control

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pyb import Pin, ExtInt, Timer
 pm1 = Pin('X1') # X1 has TIM2, CH1
 tim2 = Timer(2, freq=1000)
 ch1 = tim2.channel(1, Timer.PWM, pin=pm1)
-ch1.pulse_width_percent(0)
+ch1.pulse_width_percent(30)
 
 
 callback_m1c1 = lambda e: print("intr_m1c1")
@@ -13,3 +13,7 @@ ext_m1c1 = ExtInt(Pin('Y1'), ExtInt.IRQ_RISING, Pin.PULL_NONE, callback_m1c1)
 
 callback_m1c2 = lambda e: print("intr_m1c2")
 ext_m1c2 = ExtInt(Pin('Y2'), ExtInt.IRQ_RISING, Pin.PULL_NONE, callback_m1c2)
+
+# Set a target speed in revs per second
+# Set an interrupt to reset the encoder counter each second
+# while loop increasing pulse width each iteration until target speed is met
